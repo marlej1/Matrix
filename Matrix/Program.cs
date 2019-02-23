7d6b7d77a1763c8c366db6ad17f04300e993e7b7
Marcin Lejko <marcin.lejko@gmail.com>
2019-02-23 21:32:48 +0100

    commit from inny matrix

--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +16,7 @@ namespace ConsoleApplication1
             Start();
             Console.ReadKey();
 			Console.ReadKey();
+Console.ReadKey();
 			
         }
 
